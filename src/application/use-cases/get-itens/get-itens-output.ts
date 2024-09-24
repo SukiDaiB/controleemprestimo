80c2b40686d80b1784c55adfae26064bcce90fd7
Victor Rubert Andriolli <victorlrandriolli@gmail.com>
2024-09-24 19:29:07 -0300

    commit get itens uc

--- src/application/use-cases/get-itens/get-itens-output.ts
+++ src/application/use-cases/get-itens/get-itens-output.ts
@@ -1,3 +1,9 @@
-export type GetItensOutput ={
-    
+type ItemTypeOutput = {
+    id: string;
+    name: string;
+}
+export type GetItensOutput = {
+    id: string;
+    name: string;
+    itemType: ItemTypeOutput;
 }
--- src/application/use-cases/get-itens/get-itens-usecase.ts
+++ src/application/use-cases/get-itens/get-itens-usecase.ts
@@ -1,10 +1,26 @@
+import { TipoItem } from "../../../domain/entity/tipo-item";
 import { ItemRepository } from "../../../domain/repository/item-repository";
 import { GetItensInput } from "./get-itens-input";
 import { GetItensOutput } from "./get-itens-output";
 
 export class GetItensUseCase{
     constructor(readonly itemRepository: ItemRepository){}
-    execute(input: GetItensInput): GetItensOutput{
-        return {}
+    execute(input: GetItensInput): GetItensOutput[]{
+        const listaDeItens = this.itemRepository.getAll();
+
+        const output: GetItensOutput[] = [];
+
+        for(const itemdalista of listaDeItens){
+            output.push{
+                {
+                    id: itemdalista.getId(),
+                    name: itemdalista.getName(),
+                    tipoItem: {
+                        id: itemdalista.getTipoItem
+                    }
+                }
+            }
+        }
+        
     }
 }
