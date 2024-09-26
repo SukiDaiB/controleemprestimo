b40478afe3cc3261fbac29436361a0c1f7a554a3
SukiDaiB <brunozosteffen@gmail.com>
2024-09-26 00:58:08 +0000

    criei o output e iniciei a modelagem do usecase

--- src/application/use-cases/create-emprestimo/create-emprestimo-usecase-output.ts
+++ src/application/use-cases/create-emprestimo/create-emprestimo-usecase-output.ts
@@ -1,3 +1,30 @@
-export type CreateEmprestimoOutput = {
+type UsuarioOutput = {
+    id: string,
+    nomeUsuario: string,
+    pessoa: PessoaOutput
+}
 
+type PessoaOutput = {
+    id: string,
+    nome: string
+}
+
+type TipoItemOutput = {
+    id: string,
+    nome: string
+}
+
+type ItemOutput = {
+    id: string,
+    nome: string
+    tipoItem: TipoItemOutput
+}
+
+export type CreateEmprestimoOutput = {
+    id: string,
+    item: ItemOutput,
+    dataEmprestimo: string,
+    dataDevolucao: string,
+    pessoa: PessoaOutput,
+    usuario: UsuarioOutput
 }
--- src/application/use-cases/create-emprestimo/create-emprestimo-usecase.ts
+++ src/application/use-cases/create-emprestimo/create-emprestimo-usecase.ts
@@ -5,6 +5,27 @@ import { CreateEmprestimoOutput } from "./create-emprestimo-usecase-output";
 export class CreateEmprestimoUseCase{
     constructor(readonly emprestimoRepository: EmprestimoRepository) {}
     execute(input: CreateEmprestimoInput):CreateEmprestimoOutput{
+        const emprestimo = this.emprestimoRepository.();
+        
+        const output: CreateEmprestimoOutput = {
+            id: emprestimo.getId(),
+            item: emprestimo.
+        }
+        
         return {}
     }
 }
+
+
+//const usuario = this.usuarioRepository.getById(input.id);
+//
+//const output: GetUsuarioOutput = {
+//    id: usuario.getId(),
+//    username: usuario.getUsername(),
+//    pessoa: {
+//        id: usuario.getPessoa().getId(),
+//        name: usuario.getPessoa().getName()
+//   }
+//}
+
+//return output;
